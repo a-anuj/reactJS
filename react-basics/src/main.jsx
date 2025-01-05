c8f9f9d113a89bf6423e69a768bc15ffacf9a6d6
a-anuj <anujamirthalingam5@gmail.com>
2025-01-05 20:53:16 +0530

    added styles to classes

--- react-basics/src/main.jsx
+++ react-basics/src/main.jsx
@@ -5,8 +5,15 @@ const root = createRoot(document.getElementById("root"))
 
 function Header(){
   return (
-  <header>
-        <img src="/src/assets/images/React.js_logo-512.webp" alt="React logo" width="50px" />
+  <header className='header'>
+        <img className='nav-logo' src="/src/assets/images/React.js_logo-512.webp" alt="React logo" />
+        <nav>
+          <ul className='nav-list'>
+            <li className='nav-list-item'>Pricing</li>
+            <li className='nav-list-item'>About</li>
+            <li className='nav-list-item'>Contact</li>
+          </ul>
+        </nav>
   </header>
   )
 }
@@ -25,11 +32,16 @@ function MainContent(){
 }
 
 function Footer(){
+  return (
   <>
-    <footer>
+  <div className='div-footer'>
+    <footer className='footer'>
       <small>©️ 2024 React Development. All rights reserved.</small>
     </footer>
+  </div>
+    
   </>
+  )
 }
 
 function Page(){
