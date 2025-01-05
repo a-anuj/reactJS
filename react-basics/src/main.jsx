20a2462277acfbd49fa546113aa60bef40bcd46b
a-anuj <anujamirthalingam5@gmail.com>
2025-01-05 14:32:58 +0530

    created the structure for the basic static react project

--- react-basics/src/main.jsx
+++ react-basics/src/main.jsx
@@ -7,8 +7,15 @@ const root = createRoot(document.getElementById("root"))
 
 const testElement = 
 <div>
-  <h1>Hello from react world!!</h1>
-  <a href="https://www.google.com">Sample Link</a>
+  <img src="/src/assets/images/React.js_logo-512.webp" width="40px" alt="" />
+  <h1>Fun Facts about React</h1>
+  <ul>
+    <li>Was released in 2013</li>
+    <li>Was originally created by Jordan Walke</li>
+    <li>Has well over 100k stars on github</li>
+    <li>It is maintained by meta</li>
+    <li>Power thousands of enterprise apps</li>
+  </ul>
 </div>
 
 root.render(testElement)
