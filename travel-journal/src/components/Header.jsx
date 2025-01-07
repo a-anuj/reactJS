ab4a442a19100b40fd89183bd7a4ff18fe4fa4ba
a-anuj <anujamirthalingam5@gmail.com>
2025-01-07 18:25:26 +0530

    done with the header part of the travel-journal

---
+++ travel-journal/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return (
+        <header>
+            <img src="src/assets/images/globe.png" alt="globe" />
+            <h1>my travel journey</h1>
+        </header>
+    )
+}
---
+++ travel-journal/src/main.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom'
+import './index.css'
+import App from './App'
+
+const root = createRoot(document.getElementById("root"))
+root.render(
+    <App />
+)
