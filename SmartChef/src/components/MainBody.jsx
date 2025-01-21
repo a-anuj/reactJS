8a0c98bf9e7a76d317a6a27ff11ed976ce554970
a-anuj <anujamirthalingam5@gmail.com>
2025-01-21 20:44:17 +0530

    map the ingredients list

--- SmartChef/src/components/MainBody.jsx
+++ SmartChef/src/components/MainBody.jsx
@@ -1,10 +1,20 @@
 export default function MainBody(){
+    const ingredients = ["Onion","Tomato","Paneer"]
+
+    const ingredientList = ingredients.map(ingredient => (
+        <li>{ingredient}</li>
+    ))
+
+    
     return(
         <main>
             <form className="add-ingredient-form">
                 <input type="text" aria-label="Add ingredient" placeholder="e.g. tomato"/>
                 <button>Add ingredient</button>
             </form>
+            <ul>
+                {ingredientList}
+            </ul>
         </main>
     )
 }
